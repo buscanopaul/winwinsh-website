6be229394d0a9219f2b7aa97b6b94d8cc0476a3b
Paul Buscano <pbuscano@propertyvista.com>
2023-09-08 00:38:04 +0800

    [ADDED] footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,60 @@
 import {useMatches, NavLink} from '@remix-run/react';
 import type {FooterQuery} from 'storefrontapi.generated';
+import Facebook from '../../public/facebook.png';
+import Instagram from '../../public/instagram.png';
+import Tiktok from '../../public/tiktok.png';
+import Tools from '../../public/tools.png';
 
 export function Footer({menu}: FooterQuery) {
   return (
     <footer className="footer">
-      <FooterMenu menu={menu} />
+      <div className="px-5 bg-[length:20vw_20vw] background-grid-gray flex flex-col items-center pt-20">
+        <div className="z-10 flex flex-col items-center">
+          <h3 className="text-white font-bold">WinWish Auto Shop</h3>
+          <h5 className="text-sm text-white font-bold">EST | 2022</h5>
+          <img src={Tools} width={30} height={30} className="mb-5" />
+          <h2 className="text-white text-5xl">(02) 358 7135</h2>
+          <div className="text-white flex flex-col items-center mb-20">
+            <p className="text-[10px]">MONDAY - FRIDAY 7:00 AM - 7:00 PM</p>
+            <p className="text-[10px]">
+              UNIT 10 NORTHCREST VILLAGE, BAGUMBONG CALOOCAN CITY
+            </p>
+            <p className="text-[10px]">WINWISHAUTOSHOP@GMAIL.COM</p>
+          </div>
+          <div className="flex flex-row gap-5">
+            <a href="https://www.facebook.com/winwishph" target="_blank">
+              <img src={Facebook} width={50} height={50} />
+            </a>
+            <a href="https://www.instagram.com/winwishph" target="_blank">
+              <img src={Instagram} width={50} height={50} />
+            </a>
+            <a href="https://www.tiktok.com/winwishph" target="_blank">
+              <img src={Tiktok} width={50} height={50} />
+            </a>
+          </div>
+        </div>
+        <div className="flex md:flex-row flex-col items-center justify-between w-full mt-20 mb-10 gap-2 md:gap-0">
+          <p className="text-white opacity-50 text-[10px]">
+            &copy; 2023 WinWish Auto Shop
+          </p>
+          <a
+            href="https://paulbuscano.vercel.app"
+            target="_blank"
+            className="underline flex flex-row group"
+          >
+            <p className="text-white opacity-50 text-[10px] underline group-hover:opacity-100 transition">
+              Built with
+            </p>
+            <p className="text-red-500 text-[10px] underline">&#9829;</p>
+            <p className="text-white opacity-50 text-[10px] underline group-hover:opacity-100 transition">
+              by Paul Buscano
+            </p>
+          </a>
+          <p className="text-white opacity-50 text-[10px]">
+            Inc. All Rights Reserved.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
